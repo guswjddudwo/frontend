5218b092671486752128408ff881de7e468699ae
guswjddudwo <guswjddudwo@gmail.com>
2023-01-19 14:28:26 +0900

    /.

--- 11_react/my-app/src/index.js
+++ 11_react/my-app/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import JsxUse from './chapter3/jsxUse';
+// import JsxUse from './chapter3/3.4/JsxUse';
 import Library from './chapter3/Library';
 import Clock from './chapter4/Clock';
+import PropUse from './chapter5/5.3/PropUse';
+import CommentList from './chapter5/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -20,11 +22,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // 4장 예제
 // 1초 마다 Clock 컴포넌트를 root div에 렌더링 하는 코드
-setInterval(() => {
-  root.render(
-    <Clock />
-  );
-}, 1000);
+// setInterval(() => {
+//   root.render(
+//     <Clock />
+//   );
+// }, 1000);
+
+// 5장 예제
+root.render(
+  // <PropUse />
+  <CommentList />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
