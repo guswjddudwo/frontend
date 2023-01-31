cbd1dabf72de989db0acac99fc3bc0bbfc76f0fe
guswjddudwo <guswjddudwo@gmail.com>
2023-01-31 15:38:11 +0900

    /

--- 11_react/my-post/src/App.js
+++ 11_react/my-post/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const [likeCount, setLikeCount] = useState([0, 0, 0]);
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <>
@@ -56,17 +57,71 @@ function App() {
               <p>by goni.kim</p>
 
               <hr />
+              <div className='toolbar'>
+              {/* 좋아요 버튼 */}
+              <span onClick={(e) => {
+                // (버그수정) 현재는 좋아요 버튼 누를때 포스느 상세보기까지 같이 클릭됨
+                // 부모-자식 관계에 있을 때 이벤트 버블링이 일어남
 
-              <span onClick={() => {
+                e.stopPropagation() // 상위요소로 전파되는 이벤트 버블링을 막고 싶을 때
+
                 const copyLikeCount = [...likeCount];
                 // TODO: 
-                copyLikeCount[index]+=1;
+                copyLikeCount[index] ++;
+                setLikeCount(copyLikeCount);
               }}
               >💍 {likeCount[index]}</span>
 
+              {/* Q. 포스트마다 삭제 버튼 & 기능 만들기 */}
+              <span style={{fontSize: 27}}
+              onClick={(e) => {
+                e.stopPropagation();
+
+                // const copyPosts = [...posts];
+                // copyPosts.splice(index,1);
+                // setPosts(copyPosts);
+
+                // 또는 배열의 filter() 함수 사용
+                const filteredPosts = posts.filter((value,idx) => { 
+                  return index !== idx;
+                });
+                setPosts(filteredPosts);
+
+                // 삭제 시 해당 포스트의 좋아요 카운트도 같이 삭제
+                const copyLikeCount = [...likeCount];
+                copyLikeCount.splice(index,1);
+                setLikeCount(copyLikeCount);
+              }}
+              >🗑 </span>
+
+              </div>
             </div>
           );
         })}
+
+          
+
+        {/* 새로운 포스트 등록 */}
+        <input type="text" value={inputValue} onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        />
+        <button type='button' onClick={(e) => {
+          // post state에 요소 하나 추가하면 자동으로 렌더링 됨
+          const copyPost = [...posts];
+          copyPost.unshift(inputValue);
+          setPosts(copyPost)
+          setInputValue('');
+
+          // 포스트 하나 추가 시 좋아요 카운트도 같이 추가
+          const copyLikeCount = [...likeCount];
+          copyLikeCount.unshift(0);
+          setLikeCount(copyLikeCount);
+
+
+        }}>
+          포스트 등록
+        </button>
 
 
         {/* 포스트 상세보기 조건부 렌더링 */}
